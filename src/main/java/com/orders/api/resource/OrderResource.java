18738846b49c43e312296fbe7214d69ed424296d
pierrialexandervidmar <pierri@hotmail.com.br>
2025-05-20 22:13:13 -0300

    feat: preparado o docker

---
+++ src/main/java/com/orders/api/resource/OrderResource.java
@@ -0,0 +1,47 @@
+package com.orders.api.resource;
+
+import com.orders.api.dto.CreateOrderDto;
+import com.orders.api.entity.Order;
+import com.orders.api.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderResource {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderResource(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+//    @PostMapping
+//    public Order create(@RequestBody CreateOrderDto createOrderDto) {
+//        return orderService.create(createOrderDto);
+//    }
+//
+//    @GetMapping
+//    public List<Order> findAll() {
+//        return orderService.findAll();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Order findOne(@PathVariable UUID id) {
+//        return orderService.findOne(id);
+//    }
+
+//    @PutMapping("/{id}")
+//    public Order update(@PathVariable UUID id, @RequestBody OrderDto updateOrderDto) {
+//        return orderService.update(id, updateOrderDto);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void delete(@PathVariable UUID id) {
+//        orderService.delete(id);
+//    }
+}
---
+++ src/main/java/com/orders/api/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.orders.api.service;
+
+import com.orders.api.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+
+}
